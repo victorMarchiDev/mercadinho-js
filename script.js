979b6b4e7ea3ec0402c859a5648f5ab530ec4283
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-06-03 20:21:23 -0300

    consumindo a API fakestore e fazendo primeiros testes

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Oii");
+
+const divCatalogo = document.getElementById('catalogo');
+
+async function fetchProdutos(){
+    try{
+      const resposta =  await fetch('https://fakestoreapi.com/products');
+      const listaProdutos = resposta.json();
+      console.log(listaProdutos);
+    } catch (error) {
+        console.error("Erro na requisição da API", error);
+        divCatalogo.innerHTML = "<h1>Site indisponível, tente novamente</h1>"
+    }
+}
+
+fetchProdutos();
